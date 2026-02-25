84a25f852ecb46677d97e3c6894660bc8d05fcca
Jamie <jperrelet@gmail.com>
2026-02-25 15:47:06 -0800

    fix: `wp_normalize_path` in `get_variables` for windows

--- include/model/precompiler.class.php
+++ include/model/precompiler.class.php
@@ -341,9 +341,9 @@ class Precompiler {
 		if (is_null($this->variables)) {
 
 			$this->variables = apply_filters('sassy-variables', [
-				'wp-content-url' => '"'. WP_CONTENT_URL . '"',
-				'template-directory-url'   => '"'. get_template_directory_uri() . '"',
-				'stylesheet-directory-url' => '"'. get_stylesheet_directory_uri() . '"',
+				'wp-content-url' => '"'. wp_normalize_path(WP_CONTENT_URL) . '"',
+				'template-directory-url'   => '"'. wp_normalize_path(get_template_directory_uri()) . '"',
+				'stylesheet-directory-url' => '"'. wp_normalize_path(get_stylesheet_directory_uri()) . '"',
 			], $this->src, $this->handle, $this);
 
 		}
